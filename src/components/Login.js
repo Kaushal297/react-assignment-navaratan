79bb3f237a58c31e5647a28a7e49515545b366fa
Kaushal Kumar <kaushalkumar297@gmail.com>
2022-04-07 12:15:57 +0530

    documentation added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,19 @@
 import styles from "../styles/login.module.css";
+import { useState } from "react";
+import { Route } from "react-router-dom";
 
 function Login() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [isChecked, setIsChecked] = useState(false);
+
+	const handlelogin = () => {
+		if (username !== "" && password !== "") {
+			// TODO: push it to order page
+		} else {
+			// TODO: throw/display the errors
+		}
+	};
 	return (
 		<div
 			className={`${styles.container} flex flex-col items-center justify-center`}>
@@ -9,19 +22,37 @@ function Login() {
 				name='username'
 				id='username'
 				placeholder='Username'
+				value={username}
+				onChange={(e) => {
+					setUsername(e.target.value);
+				}}
+				required
 			/>
 			<input
 				type='password'
 				name='password'
 				id='password'
 				placeholder='Password'
+				value={password}
+				onChange={(e) => {
+					setPassword(e.target.value);
+				}}
+				required
 			/>
 			<div>
-				<input type='checkbox' name='remember me' id='rememberMe' />
+				<input
+					type='checkbox'
+					name='remember me'
+					id='rememberMe'
+					checked={isChecked}
+					onChange={() => {
+						setIsChecked(!isChecked);
+					}}
+				/>
 				<label htmlFor='rememberMe'>Remember Me</label>
 			</div>
 
-			<button>login</button>
+			<button onClick={handlelogin}>login</button>
 		</div>
 	);
 }
